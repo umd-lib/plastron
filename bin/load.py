cc5ae6d37ef549ef02b9ad8678f03cbc545e0158
jwestgard <jwestgard@mac.com>
2016-07-19 16:53:08 -0400

    set up batch class

--- bin/load.py
+++ bin/load.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
+from __future__ import print_function
+
 import argparse
-import urllib2
-import rdflib
+import logging
+import lxml.etree as ET
 import os
+import rdflib
+import requests
 import sys
 import yaml
 
 
-def load_config(configfile):
-    print('Loading configuration from "{0}" ... '.format(configfile), end='')
-    with open(schemafile, 'r') as f:
-        config = yaml.load(f)
-    print("success.")
-    return config
+class batch(path):
+    '''class representing the set of resources to be loaded'''
+    def __init__(self, path):
+        self.parse(filepath, format='turtle')
+        self.filename = os.path.basename(filepath)
+        self.uri = uri
+        print(" - Resource {0} has {1} triples.".format(
+            self.filename, len(self))
+            )
+    
+
+
 
 
 def main():
@@ -22,11 +32,13 @@ def main():
     parser = argparse.ArgumentParser(
         description='Newspaper Batch Loader for Fedora 4.'
         )
-    parser.add_argument("config")
+    parser.add_argument("--config", "-c", action="store")
     parser.add_argument("path")
     
     args = parser.parse_args()
 
+    with open(args.config, 'r') as configfile:
+        globals().update(yaml.safe_load(configfile))
 
 
 if __name__ == "__main__":

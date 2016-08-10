7a35a19c43c9b33e7e91a4949b8690195862c11e
jwestgard <jwestgard@mac.com>
2016-08-10 18:24:38 -0400

    refactor ndnp traversal to separate module

--- bin/load.py
+++ bin/load.py
@@ -5,14 +5,12 @@ from __future__ import print_function
 
 import argparse
 import logging
-import lxml.etree as ET
-import os
 import rdflib
 import requests
 import sys
 import yaml
 
-import metadata_map
+from ndnp import *
 
 #============================================================================
 # HELPER FUNCTIONS 
@@ -23,89 +21,6 @@ def print_header():
     title = 'FCREPO BATCH LOADER'
     bar = '=' * len(title)
     print('\n'.join([" ", title, bar]))
-
-
-#============================================================================
-# CLASSES
-#============================================================================
-
-class batch():
-    '''class representing the set of resources to be loaded'''
-    def __init__(self, batchfile):
-        tree = ET.parse(batchfile)
-        root = tree.getroot()
-        md = metadata_map.batch
-        
-        self.basepath = os.path.dirname(batchfile)
-        self.issues = root.findall(md['issues'])
-        print("Batch contains {} issues: ".format(len(self.issues)))
-        for n, issue in enumerate(self.issues):
-            print("\n\n ISSUE {}: ".format(n+1))
-            i = item(os.path.join(self.basepath, issue.text))
-            
-            
-
-class item():
-    '''class representing the files of an individual item'''
-    def __init__(self, path):
-        tree = ET.parse(path)
-        root = tree.getroot()
-        m = metadata_map.issue
-        
-        self.dir     = os.path.dirname(path)
-        self.volume  = root.find(m['volume']).text
-        self.issue   = root.find(m['issue']).text
-        self.edition = root.find(m['edition']).text
-        self.date    = root.find(m['date']).text
-        self.pages   = [
-            p for p in root.findall(m['pages']) \
-                if p.get('ID').startswith('pageModsBib')
-            ]
-        self.files   = [f for f in root.findall(m['files'])]
-        
-        header = "Diamondback {0}: Vol. {1}.{2} (ed. {3})".format(
-            self.date, self.volume, self.issue, self.edition
-            )
-        border = "=" * (len(header) + 4)
-        
-        print(border)
-        print("|", header, "|")
-        print(border)
-        
-        for n, pagexml in enumerate(self.pages):
-            id = pagexml.get('ID').strip('pageModsBib')
-            filegroup = next(
-                f for f in self.files if f.get('ID').endswith(id)
-                )
-            p = page(pagexml, filegroup)
-
-            print("  Page {0}: Reel {1}, Frame {2}".format(
-                n+1, p.reel, p.frame
-                ))
-
-            for f in p.files:
-                f.path = os.path.join(self.dir, os.path.basename(f.relpath))
-                print('   => {0}: {1}'.format(f.use, f.path))
-
-
-
-class page():
-    '''class representing the individual page'''
-    def __init__(self, pagexml, filegroup):
-        m = metadata_map.page
-        self.reel   = pagexml.find(m['reel']).text
-        self.frame  = pagexml.find(m['frame']).text
-        self.files  = [file(f) for f in filegroup.findall(m['files'])]
-        
-               
-        
-class file():
-    '''class representing the individual file'''
-    def __init__(self, filexml):
-        m = metadata_map.file
-        self.use  = filexml.get('USE')
-        elem = filexml.find(m['filepath'])
-        self.relpath = elem.get('{http://www.w3.org/1999/xlink}href')
 
 
 #============================================================================
@@ -129,6 +44,57 @@ def main():
         print("Successfully imported metadata mapping.")
     
     b = batch(args.path)
+    
+    for n, item in enumerate(b.items):
+        print("\n\n ISSUE {}: ".format(n+1))
+        header = "Diamondback {0}: Vol. {1}.{2} (ed. {3})".format(
+            item.date, item.volume, item.issue, item.edition
+            )
+        border = "=" * (len(header) + 4)
+        
+        print(border)
+        print("|", header, "|")
+        print(border)
+        
+        for n, page in enumerate(item.pages):
+            print("  Page {0}: Reel {1}, Frame {2}".format(
+                n+1, page.reel, page.frame
+                ))
+            for file in page.files:
+                print('   => {0}: {1}'.format(file.use, file.path))
+        
+        print("\nCreating LDP Container...")
+        response = requests.post(REST_ENDPOINT)
+        if response.status_code == 201:
+            uri = response.text
+            print("  {0} Created! => {1}".format(response, uri))
+        
+        print("\nCreating RDF Graph...")
+        props = rdflib.Graph()
+        
+        
+        
+        '''
+        Create collection (c) and set up ACL
+            <> a PCDM:Collection
+            
+        For each item (i) in collection:
+            c hasMember i
+            <> a pcdm:Object
+            <> pcdm:memberOf c
+            
+            for each page (p) in i
+                <> a pcdm:Object
+                
+                for each file (f) in p
+                    <> a pcdm:file
+                    <> pcdm:fileOf
+                
+                    attach binary to pcdm:File
+        
+        '''
+        
+
 
 
 if __name__ == "__main__":
---
+++ bin/ndnp.py
@@ -0,0 +1,136 @@
+'''Classes for interpreting and loading metadata and files stored 
+according to the NDNP specification.'''
+
+import lxml.etree as ET
+import os
+
+#============================================================================
+# METADATA MAPPING
+#============================================================================
+
+METAMAP = {
+    'batch': {
+        'issues':   "./{http://www.loc.gov/ndnp}issue"
+        },
+        
+    'issue': {
+        'volume':   (".//{http://www.loc.gov/mods/v3}detail[@type='volume']/"
+                    "{http://www.loc.gov/mods/v3}number"
+                    ),
+        'issue':    (".//{http://www.loc.gov/mods/v3}detail[@type='issue']/"
+                    "{http://www.loc.gov/mods/v3}number"
+                    ),
+        'edition':  (".//{http://www.loc.gov/mods/v3}detail[@type='edition']/"
+                    "{http://www.loc.gov/mods/v3}number"
+                    ),
+        'date':     (".//{http://www.loc.gov/mods/v3}dateIssued"
+                    ),
+        'lccn':     (".//{http://www.loc.gov/mods/v3}"
+                    "identifier[@type='lccn']"
+                    ),
+        'pages':    (".//{http://www.loc.gov/METS/}dmdSec"
+                    ),
+        'files':    (".//{http://www.loc.gov/METS/}fileGrp"
+                    ),
+        },
+        
+    'page': {
+        'number':   (".//{http://www.loc.gov/mods/v3}start"
+                    ),
+        'reel':     (".//{http://www.loc.gov/mods/v3}"
+                    "identifier[@type='reel number']"
+                    ),
+        'location': (".//{http://www.loc.gov/mods/v3}physicalLocation"
+                    ),
+        'frame':    (".//{http://www.loc.gov/mods/v3}"
+                    "identifier[@type='reel sequence number']"
+                    ),
+        'files':    (".//{http://www.loc.gov/METS/}file"
+                    ),
+        },
+        
+    'file': {
+        'number':   (".//{http://www.loc.gov/mods/v3}start"
+                    ),
+        'filepath': (".//{http://www.loc.gov/METS/}FLocat"
+                    ),
+        }
+    }
+
+
+#============================================================================
+# CLASSES
+#============================================================================
+
+class batch():
+    '''class representing the set of resources to be loaded'''
+    def __init__(self, batchfile):
+        tree = ET.parse(batchfile)
+        root = tree.getroot()
+        m = METAMAP['batch']
+        
+        self.basepath = os.path.dirname(batchfile)
+        self.paths = [
+            os.path.join(self.basepath, 
+                i.text) for i in root.findall(m['issues'])
+            ]
+        self.num_items = len(self.paths)
+        
+        print("Batch contains {} issues.".format(len(self.paths)))
+        
+        self.items = []
+        for n, p in enumerate(self.paths):
+            print("Processing item {0}/{1}...".format(n+1, 
+                    self.num_items), end='\r')
+            self.items.append(item(p))
+        
+        print('\nDone!')
+
+
+class item():
+    '''class representing the pieces of an individual item'''
+    def __init__(self, path):
+        tree = ET.parse(path)
+        root = tree.getroot()
+        m = METAMAP['issue']
+        
+        self.dir     = os.path.dirname(path)
+        self.volume  = root.find(m['volume']).text
+        self.issue   = root.find(m['issue']).text
+        self.edition = root.find(m['edition']).text
+        self.date    = root.find(m['date']).text
+        self.pages   = []
+        
+        filexml_snippets = [f for f in root.findall(m['files'])]
+        pagexml_snippets = [p for p in root.findall(m['pages']) if \
+            p.get('ID').startswith('pageModsBib')
+            ]
+        
+        for n, pagexml in enumerate(pagexml_snippets):
+            id = pagexml.get('ID').strip('pageModsBib')
+            filexml = next(
+                f for f in filexml_snippets if f.get('ID').endswith(id)
+                )
+            p = page(pagexml, filexml)
+            self.pages.append(p)
+            for f in p.files:
+                f.path = os.path.join(self.dir, os.path.basename(f.relpath))
+
+
+class page():
+    '''class representing the individual page'''
+    def __init__(self, pagexml, filegroup):
+        m = METAMAP['page']
+        self.reel   = pagexml.find(m['reel']).text
+        self.frame  = pagexml.find(m['frame']).text
+        self.files  = [file(f) for f in filegroup.findall(m['files'])]
+        
+        
+class file():
+    '''class representing the individual file'''
+    def __init__(self, filexml):
+        m = METAMAP['file']
+        self.use  = filexml.get('USE')
+        elem = filexml.find(m['filepath'])
+        self.relpath = elem.get('{http://www.w3.org/1999/xlink}href')
+        

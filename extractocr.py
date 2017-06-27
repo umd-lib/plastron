18ad9acf9a0933c8100a04f30f83e2d92aca4a8b
Peter Eichman <peichman@umd.edu>
2017-06-27 16:37:02 -0400

    LIBFCREPO-366. Handle exceptions when looping over URIs.
    Unless the exception occurred when attempting to commit or rollback the transaction, record the skipped URI and continue.
    
    https://issues.umd.edu/browse/LIBFCREPO-366

--- extractocr.py
+++ extractocr.py
@@ -11,8 +11,32 @@ from handler import ndnp
 import rdflib
 from rdflib import RDF
 from lxml import etree as ET
+import os
+import csv
 
 logger = logging.getLogger(__name__)
+
+def extract(fcrepo, uri):
+    fcrepo.open_transaction()
+    try:
+        logger.info("Getting {0} from repository".format(uri))
+        page = ndnp.Page.from_repository(fcrepo, uri)
+        logger.info("Creating annotations for page {0}".format(page.title))
+        for annotation in page.textblocks():
+            annotation.create_object(fcrepo)
+            annotation.update_object(fcrepo)
+
+        fcrepo.commit_transaction()
+        return True
+
+    except (pcdm.RESTAPIException, ndnp.DataReadException) as e:
+        # if anything fails during item creation or commiting the transaction
+        # attempt to rollback the current transaction
+        # failures here will be caught by the main loop's exception handler
+        # and should trigger a system exit
+        logger.error("Item creation failed: {0}".format(e))
+        fcrepo.rollback_transaction()
+        logger.warn('Transaction rolled back. Continuing load.')
 
 def main():
     '''Parse args and handle options.'''
@@ -57,22 +81,34 @@ def main():
 
     logger.info('Found {0} completed items'.format(len(completed)))
 
+    skipfile = os.path.join('logs/skipped.csv')
+    sfile = open(skipfile, 'w+', 1)
+    skip_writer = csv.DictWriter(sfile, fieldnames=['uri', 'timestamp'])
+    skip_writer.writeheader()
+
     with fcrepo.at_path('/annotations'):
         for uri in args.uris:
             if uri in completed:
                 continue
 
-            fcrepo.open_transaction()
-            page = ndnp.Page.from_repository(fcrepo, uri)
-            logger.info("Creating annotations for page {0}".format(page.title))
-            for annotation in page.textblocks():
-                annotation.create_object(fcrepo)
-                annotation.update_object(fcrepo)
-            fcrepo.commit_transaction()
-            completed.writerow({
+            is_extracted = False
+            try:
+                is_extracted = extract(fcrepo, uri)
+            except pcdm.RESTAPIException as e:
+                logger.error(
+                    "Unable to commit or rollback transaction, aborting"
+                    )
+                sys.exit(1)
+
+            row = {
                 'uri': uri,
                 'timestamp': str(datetime.utcnow())
-                })
+                }
+
+            if is_extracted:
+                completed.writerow(row)
+            else:
+                skip_writer.writerow(row)
 
 if __name__ == "__main__":
     main()

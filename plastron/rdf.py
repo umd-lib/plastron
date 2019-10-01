9d4ec5adffd556fb5b8b04fd0e1ade89cc3bdc36
Peter Eichman <peichman@umd.edu>
2019-10-01 11:57:19 -0400

    LIBFCREPO-695. Content-model-based CSV export.
    * Moved serializer classes into plastron.serializers module
    * Omit server-managed triples from export command
    * Add RDF model capability to read objects from a graph
    * Object properties can have an obj_class
    * Embedded object_properties with an obj_class get read into the model at the same time as their parent object
    * Used a metaclass to make the RDF property inheritance work correctly
    * Made values() method a generator
    * Include subject URI and index of hash URIs in CSV output
    * Always back properties with a values array
    * Added Letter and Poster models
    * Add additional language columns when serializing to CSV
    * Remove completely unused columns from CSV export
    * Auto-detect the plastron model class to use based on the RDF type of the graph
    
    https://issues.umd.edu/browse/LIBFCREPO-695

--- plastron/rdf.py
+++ plastron/rdf.py
@@ -8,45 +8,57 @@ ns = rdf
 
 
 def init_class_attr(cls, name, default):
-    if name not in cls.__dict__:
-        if hasattr(cls, name):
-            # there's a attribute set somewhere in the inheritance hierarchy
-            # copy it as the basis for this class's instance of that attribute
-            setattr(cls, name, copy(getattr(cls, name)))
-        else:
-            setattr(cls, name, default)
+    if hasattr(cls, name):
+        # there's a attribute set somewhere in the inheritance hierarchy
+        # copy it as the basis for this class's instance of that attribute
+        setattr(cls, name, copy(getattr(cls, name)))
+    else:
+        setattr(cls, name, default)
 
 
 def rdf_class(*types):
     def add_types(cls):
+        cls.rdf_types.update(types)
+        return cls
+    return add_types
+
+
+# metaclass that makes copies of specific class attributes
+# as we go down the inheritance hierarchy
+class Meta(type):
+    def __new__(mcs, name, bases, dct):
+        cls = super().__new__(mcs, name, bases, dct)
         # name and URI to property type class lookups
         init_class_attr(cls, 'name_to_prop', {})
         init_class_attr(cls, 'uri_to_prop', {})
         # list of property type classes for this class
         init_class_attr(cls, 'prop_types', [])
         # RDF types
-        init_class_attr(cls, 'types', set())
-        cls.types.update(types)
+        init_class_attr(cls, 'rdf_types', set())
         return cls
-
-    return add_types
 
 
 class RDFProperty(object):
     def __init__(self):
-        if self.is_multivalued:
-            self.value = []
-        else:
-            self.value = None
+        self.values = []
 
-    def values(self):
-        if isinstance(self.value, list):
-            return self.value
+    def append(self, other):
+        self.values.append(other)
+
+    def __str__(self):
+        if len(self.values) > 0:
+            return str(self.values[0])
         else:
-            return [self.value]
+            return ''
+
+    def __iter__(self):
+        return iter(self.values)
+
+    def __len__(self):
+        return len(self.values)
 
     def triples(self, subject):
-        for value in self.values():
+        for value in self.values:
             if value is not None:
                 yield (subject, self.uri, self.get_term(value))
 
@@ -66,16 +78,6 @@ class RDFObjectProperty(RDFProperty):
             raise ValueError('Expecting a URIRef or an object with a uri attribute')
 
 
-def add_property_type(name, uri, prop_type):
-    def add_property(cls):
-        cls.name_to_prop[name] = prop_type
-        cls.uri_to_prop[uri] = prop_type
-        cls.prop_types.append(prop_type)
-        return cls
-
-    return add_property
-
-
 def data_property(name, uri, multivalue=False, datatype=None):
     def add_property(cls):
         type_name = f'{cls.__name__}.{name}'
@@ -93,43 +95,11 @@ def data_property(name, uri, multivalue=False, datatype=None):
     return add_property
 
 
-def object_property(name, uri, multivalue=False, embed=False):
-    def add_property(cls):
-        type_name = f'{cls.__name__}.{name}'
-        prop_type = type(type_name, (RDFObjectProperty,), {
-            'name': name,
-            'uri': uri,
-            'is_multivalued': multivalue,
-            'is_embedded': embed
-        })
-        cls.name_to_prop[name] = prop_type
-        cls.uri_to_prop[uri] = prop_type
-        cls.prop_types.append(prop_type)
-        return cls
-
-    return add_property
-
-
-@rdf_class()
-class Resource(object):
+class Resource(metaclass=Meta):
 
     @classmethod
     def from_graph(cls, graph, subject=None):
-        return cls.from_triples(graph.triples((subject, None, None)))
-
-    @classmethod
-    def from_triples(cls, triples):
-        resource = cls()
-        for (s, p, o) in triples:
-            if p in cls.uri_to_prop:
-                prop_type = cls.uri_to_prop[p]
-                if prop_type.is_multivalued:
-                    getattr(resource, prop_type.name).append(o)
-                else:
-                    setattr(resource, prop_type.name, o)
-            else:
-                resource.unmapped_triples.append((s, p, o))
-        return resource
+        return cls(uri=subject).read(graph)
 
     def __init__(self, uri='', **kwargs):
         self.uri = URIRef(uri)
@@ -147,19 +117,38 @@ class Resource(object):
                     raise TypeError(f"Multivalued field '{key}' expects a list")
             setattr(self, key, value)
 
+    def read(self, graph):
+        for (s, p, o) in graph.triples((self.uri, None, None)):
+            if p in self.uri_to_prop:
+                prop_type = self.uri_to_prop[p]
+                if issubclass(prop_type, RDFObjectProperty) and prop_type.obj_class is not None:
+                    obj = prop_type.obj_class(uri=o)
+                    # recursively read embedded objects whose triples should be part of the same graph
+                    if prop_type.is_embedded:
+                        obj.read(graph)
+                else:
+                    obj = o
+                getattr(self, prop_type.name).append(obj)
+            else:
+                self.unmapped_triples.append((s, p, o))
+        return self
+
     def __getattr__(self, name):
         if name in self.name_to_prop:
-            return self.props[name].value
+            return self.props[name]
         else:
             raise AttributeError(f"No predicate mapped to {name}")
 
     def __setattr__(self, name, value):
         if name in self.name_to_prop:
-            self.props[name].value = value
+            if not isinstance(value, list):
+                value = [value]
+            # TODO: wrap these values in rdflib classes?
+            self.props[name].values = value
         else:
             # attribute names that aren't mapped to RDF properties
             # just get set like normal attributes
-            super(Resource, self).__setattr__(name, value)
+            super().__setattr__(name, value)
 
     def properties(self):
         return [prop for prop in self.props.values()]
@@ -189,7 +178,7 @@ class Resource(object):
     def graph(self, nsm=None):
         subject = URIRef(self.uri)
         graph = Graph(namespace_manager=nsm)
-        for rdf_type in self.types:
+        for rdf_type in self.rdf_types:
             graph.add((subject, RDF.type, rdf_type))
 
         for prop in self.properties():
@@ -200,7 +189,8 @@ class Resource(object):
         # for this resource; typically, these are resources that are have
         # hash URI identifiers
         for obj in self.embedded_objects():
-            graph = graph + obj.graph()
+            if obj is not None:
+                graph = graph + obj.graph()
 
         # any triples that were loaded from the source graph but that aren't
         # mapped to specific Python attributes
@@ -211,3 +201,20 @@ class Resource(object):
 
     def print(self, format='turtle', file=sys.stdout, nsm=None):
         print(self.graph(nsm=nsm).serialize(format=format).decode(), file=file)
+
+
+def object_property(name, uri, multivalue=False, embed=False, obj_class=None):
+    def add_property(cls):
+        type_name = f'{cls.__name__}.{name}'
+        prop_type = type(type_name, (RDFObjectProperty,), {
+            'name': name,
+            'uri': uri,
+            'is_multivalued': multivalue,
+            'is_embedded': embed,
+            'obj_class': obj_class
+        })
+        cls.name_to_prop[name] = prop_type
+        cls.uri_to_prop[uri] = prop_type
+        cls.prop_types.append(prop_type)
+        return cls
+    return add_property
---
+++ plastron/serializers.py
@@ -0,0 +1,149 @@
+import csv
+import logging
+from collections import defaultdict
+from urllib.parse import urlparse
+
+from rdflib import Literal
+
+from plastron.models.letter import Letter
+from plastron.models.poster import Poster
+from plastron.namespaces import get_manager, bibo, rdf
+from plastron.rdf import RDFObjectProperty, RDFDataProperty
+
+logger = logging.getLogger(__name__)
+nsm = get_manager()
+
+MODEL_MAP = {
+    bibo.Image: Poster,
+    bibo.Letter: Letter
+}
+
+
+class TurtleSerializer:
+    FILE_EXTENSION = 'ttl'
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __enter__(self):
+        self.fh = open(self.filename, 'wb')
+        return self
+
+    def write(self, graph):
+        graph.serialize(destination=self.fh, format='turtle')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.fh.close()
+
+
+def detect_resource_class(graph, subject):
+    types = set([o for s, p, o in graph.triples((subject, rdf.type, None))])
+
+    for rdf_type, cls in MODEL_MAP.items():
+        if rdf_type in types:
+            return cls
+    else:
+        raise Exception('Unable to detect resource type')
+
+
+class CSVSerializer:
+    FILE_EXTENSION = 'csv'
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.resource_class = None
+        self.header_map = None
+        self.headers = None
+
+    def __enter__(self):
+        self.rows = []
+        return self
+
+    def write(self, graph):
+        """
+        Serializes the given graph as CSV data rows.
+          - One row per subject, if there are multiple subjects (HashURIs)
+          - The data rows written to the csv writer with primary subject row first, followed by HashURI subject rows
+          - Appends new predicates if missing or for repeating values of the predicate to the provided headers object.
+        """
+        main_subject = set([s for s in graph.subjects() if '#' not in str(s)]).pop()
+        if self.resource_class is None:
+            self.resource_class = detect_resource_class(graph, main_subject)
+
+        self.header_map = self.resource_class.HEADER_MAP
+        self.headers = list(self.header_map.values()) + ['URI', 'INDEX']
+
+        resource = self.resource_class.from_graph(graph, subject=main_subject)
+        row = {k: ';'.join(v) for k, v in self.flatten(resource).items()}
+        row['URI'] = main_subject
+        self.rows.append(row)
+
+    LANGUAGE_NAMES = {
+        'ja': 'Japanese',
+        'ja-latn': 'Japanese (Romanized)'
+    }
+
+    def flatten(self, resource, prefix=''):
+        columns = defaultdict(lambda: [])
+        for name, prop in resource.props.items():
+            if isinstance(prop, RDFObjectProperty) and prop.is_embedded:
+                for i, obj in enumerate(prop.values):
+                    # record the list position to hash URI correlation
+                    columns['INDEX'].append(f'{name}[{i}]=#{urlparse(obj.uri).fragment}')
+                    for header, value in self.flatten(obj, prefix=f'{name}.').items():
+                        columns[header].extend(value)
+            else:
+                key = prefix + name
+                if key not in self.header_map:
+                    continue
+                header = self.header_map[key]
+
+                # create additional columns (if needed) for different languages
+                if isinstance(prop, RDFDataProperty):
+                    header_index = self.headers.index(header)
+                    per_language_columns = defaultdict(lambda: [])
+                    for value in prop.values:
+                        if isinstance(value, Literal) and value.language:
+                            language_code = value.language
+                        else:
+                            language_code = ''
+                        per_language_columns[language_code].append(value)
+                    new_headers = []
+                    for language_code, values in per_language_columns.items():
+                        serialization = '|'.join(values)
+                        if language_code:
+                            language = self.LANGUAGE_NAMES[language_code]
+                            language_header = f'{header} [{language}]'
+                            new_headers.append(language_header)
+                            columns[language_header].append(serialization)
+                        else:
+                            columns[header].append(serialization)
+                    # sort and add the new headers that have language names
+                    for i, new_header in enumerate(sorted(new_headers), start=1):
+                        self.headers.insert(header_index + i, new_header)
+                else:
+                    serialization = '|'.join(prop.values)
+                    columns[header].append(serialization)
+        return columns
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # strip out headers that aren't used in any row
+        for header in self.headers:
+            has_column_values = any([True for row in self.rows if header in row])
+            if not has_column_values:
+                self.headers.remove(header)
+        # write the CSV file
+        with open(self.filename, 'w') as fh:
+            csv_writer = csv.DictWriter(fh, self.headers, extrasaction='ignore')
+            csv_writer.writeheader()
+            for row in self.rows:
+                csv_writer.writerow(row)
+
+
+SERIALIZER_CLASSES = {
+    'text/turtle': TurtleSerializer,
+    'turtle': TurtleSerializer,
+    'ttl': TurtleSerializer,
+    'text/csv': CSVSerializer,
+    'csv': CSVSerializer
+}

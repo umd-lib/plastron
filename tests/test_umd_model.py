d7529dbdb613878e06139e6b2b588e2256dee45f
Peter Eichman <peichman@umd.edu>
2020-05-04 15:38:54 -0400

    LIBFCREPO-796. Finished identifier/accession_number test.
    https://issues.umd.edu/browse/LIBFCREPO-796

--- tests/test_umd_model.py
+++ tests/test_umd_model.py
@@ -1,5 +1,5 @@
 from plastron.models.umd import Item
-from rdflib import Graph, Literal, URIRef
+from rdflib import Graph, Literal, Namespace, URIRef
 
 rdf = (
     '@prefix dcterms: <http://purl.org/dc/terms/> .'
@@ -13,14 +13,17 @@ item = Item.from_graph(
     graph=Graph().parse(data=rdf, format='turtle', publicID=base_uri),
     subject=base_uri
 )
+dcterms = Namespace('http://purl.org/dc/terms/')
+umdtype = Namespace('http://vocab.lib.umd.edu/datatype#')
 
 def test_identifier_distinct_from_accession_number():
     assert len(item.identifier) == 1
     assert item.identifier[0] == Literal('foo')
 
     assert len(item.accession_number) == 1
-    assert item.accession_number[0] == Literal('1212XN1', datatype=URIRef('http://vocab.lib.umd.edu/datatype#accessionNumber'))
+    assert item.accession_number[0] == Literal('1212XN1', datatype=umdtype.accessionNumber)
 
     graph = item.graph()
-    assert (URIRef(base_uri), URIRef('http://purl.org/dc/terms/identifier'), Literal('foo')) in graph
+    assert (URIRef(base_uri), dcterms.identifier, Literal('foo')) in graph
+    assert (URIRef(base_uri), dcterms.identifier, Literal('1212XN1', datatype=umdtype.accessionNumber)) in graph
 

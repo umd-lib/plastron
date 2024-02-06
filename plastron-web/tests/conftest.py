afe45fb5fda5e6b5620c7f82d911b55020a39b40
Mohamed Abdul Rasheed <mohideen@umd.edu>
2024-02-06 11:52:34 -0500

    LIBFCREPO-1257. Implement tests.
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1257

---
+++ plastron-web/tests/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+
+import os
+
+def config_file_path(request):
+    test_dir = os.path.dirname(request.module.__file__)
+    return os.path.join(test_dir, "configs/plastron-config.yml")

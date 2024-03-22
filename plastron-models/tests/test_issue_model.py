add069042b097cadb4980c1d5149a8388b499338
David P. Steelman <dsteelma@umd.edu>
2024-03-22 14:25:36 -0400

    LIBFCREPO-1287. Refactored "presentation_set" tests into parameterized
    Replaced the "presentation_set"-specific tests in each of the model
    test files with a single "presentation_set"-specific test file that
    uses parameterized tests.
    
    Renamed the "test_<model>_without_presentation_set_is_valid" tests to
    "test_<model>_valid_with_only_required_fields", as that is more
    generic, and left the tests in the individual model test files (creating
    such files when necessary).
    
    The "test_issue_valid_with_only_required_fields" test in
    "test_issue_model.py" and the
    "test_poster_valid_with_only_required_fields" test in
    "test_poster_model.py" are currently set to skipped, because the
    validation for these models does not currently work (irrespective of
    the changes made in this issue).
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1287

--- plastron-models/tests/test_issue_model.py
+++ plastron-models/tests/test_issue_model.py
@@ -8,9 +8,10 @@ base_uri = 'http://example.com/xyz'
 
 
 @pytest.mark.skip('Test cannot be run because Issue validation does not currently work')
-def test_issue_without_presentation_set_is_valid():
+def test_issue_valid_with_only_required_fields():
     issue = Issue()
-    # Required fields
+
+    # Only provide required fields
     issue.identifier = 'test_issue'
     issue.title = 'Test Issue'
     issue.date = '1970-01-01'
@@ -19,81 +20,3 @@ def test_issue_without_presentation_set_is_valid():
     issue.edition = '1'
 
     assert issue.is_valid
-
-
-def test_issue_with_single_presentation_set():
-    issue = create_issue_with_presentation_set(
-        base_uri, ['foo']
-    )
-
-    assert len(issue.presentation_set) == 1
-    assert issue.presentation_set.value == URIRef('http://vocab.lib.umd.edu/set#foo')
-
-
-def test_letter_validation_with_valid_presentation_set():
-    issue = create_issue_with_presentation_set(
-        base_uri, ['test']
-    )
-    assert issue.presentation_set.is_valid
-
-
-def test_issue_validation_with_invalid_presentation_set():
-    issue = create_issue_with_presentation_set(
-        base_uri, ['not_valid']
-    )
-    assert not issue.presentation_set.is_valid
-
-
-def test_issue_with_multiple_presentation_sets():
-    issue = create_issue_with_presentation_set(
-        base_uri, ['foo', 'bar']
-    )
-
-    assert len(issue.presentation_set) == 2
-
-    expected = sorted([URIRef('http://vocab.lib.umd.edu/set#bar'), URIRef('http://vocab.lib.umd.edu/set#foo')])
-    assert sorted(list(issue.presentation_set.values)) == expected
-
-
-def test_single_presentation_set_can_be_set_on_letter():
-    issue = Issue(uri=URIRef('http://example.com/123'), presentation_set=URIRef('http://vocab.lib.umd.edu/set#foobar'))
-
-    expected = (URIRef('http://example.com/123'), ore.isAggregatedBy, URIRef('http://vocab.lib.umd.edu/set#foobar'))
-    assert expected in issue.graph
-
-
-def test_multiple_presentation_sets_can_be_set_on_issue():
-    base_uri = URIRef('http://example.com/123')
-
-    graph = Graph()
-    graph.add((base_uri, ore.isAggregatedBy, URIRef('http://vocab.lib.umd.edu/set#foobar')))
-    graph.add((base_uri, ore.isAggregatedBy, URIRef('http://vocab.lib.umd.edu/set#barbaz')))
-
-    issue = Issue(uri=base_uri, presentation_set=graph.objects())
-
-    expected = [
-        (URIRef('http://example.com/123'), ore.isAggregatedBy, URIRef('http://vocab.lib.umd.edu/set#foobar')),
-        (URIRef('http://example.com/123'), ore.isAggregatedBy, URIRef('http://vocab.lib.umd.edu/set#barbaz'))
-    ]
-
-    for e in expected:
-        assert e in issue.graph
-
-
-# Helper Functions
-
-def create_presentation_set_turtle_format(set_names):
-    preamble = '@prefix ore: <http://www.openarchives.org/ore/terms/> .'
-    preamble = preamble + '@prefix umdset: <http://vocab.lib.umd.edu/set#> .'
-    presentation_set = preamble
-    for set_name in set_names:
-        presentation_set += f'<> ore:isAggregatedBy umdset:{set_name} .'
-    return presentation_set
-
-
-def create_issue_with_presentation_set(item_uri, set_names):
-    presentation_set = create_presentation_set_turtle_format(set_names)
-    return Issue(
-        graph=Graph().parse(data=presentation_set, format='turtle', publicID=base_uri),
-        uri=item_uri
-    )
--- plastron-models/tests/test_poster_model.py
+++ plastron-models/tests/test_poster_model.py
@@ -8,9 +8,10 @@ base_uri = 'http://example.com/xyz'
 
 
 @pytest.mark.skip('Test cannot be run because Poster validation does not currently work')
-def test_poster_without_presentation_set_is_valid():
+def test_poster_valid_with_only_required_fields():
     poster = Poster()
-    # Required fields
+
+    # Only provide required fields
     poster.identifier = 'test_poster'
     poster.title = 'Test Poster'
     poster.language = 'en'
@@ -21,81 +22,3 @@ def test_poster_without_presentation_set_is_valid():
     poster.rights = 'http://rightsstatements.org/vocab/NoC-US/1.0/'
 
     assert poster.is_valid
-
-
-def test_poster_with_single_presentation_set():
-    poster = create_poster_with_presentation_set(
-        base_uri, ['foo']
-    )
-
-    assert len(poster.presentation_set) == 1
-    assert poster.presentation_set.value == URIRef('http://vocab.lib.umd.edu/set#foo')
-
-
-def test_poster_validation_with_valid_presentation_set():
-    poster = create_poster_with_presentation_set(
-        base_uri, ['test']
-    )
-    assert poster.presentation_set.is_valid
-
-
-def test_poster_validation_with_invalid_presentation_set():
-    poster = create_poster_with_presentation_set(
-        base_uri, ['not_valid']
-    )
-    assert not poster.presentation_set.is_valid
-
-
-def test_poster_with_multiple_presentation_sets():
-    poster = create_poster_with_presentation_set(
-        base_uri, ['foo', 'bar']
-    )
-
-    assert len(poster.presentation_set) == 2
-
-    expected = sorted([URIRef('http://vocab.lib.umd.edu/set#bar'), URIRef('http://vocab.lib.umd.edu/set#foo')])
-    assert sorted(list(poster.presentation_set.values)) == expected
-
-
-def test_single_presentation_set_can_be_set_on_poster():
-    poster = Poster(uri=URIRef('http://example.com/123'), presentation_set=URIRef('http://vocab.lib.umd.edu/set#foobar'))
-
-    expected = (URIRef('http://example.com/123'), ore.isAggregatedBy, URIRef('http://vocab.lib.umd.edu/set#foobar'))
-    assert expected in poster.graph
-
-
-def test_multiple_presentation_sets_can_be_set_on_poster():
-    base_uri = URIRef('http://example.com/123')
-
-    graph = Graph()
-    graph.add((base_uri, ore.isAggregatedBy, URIRef('http://vocab.lib.umd.edu/set#foobar')))
-    graph.add((base_uri, ore.isAggregatedBy, URIRef('http://vocab.lib.umd.edu/set#barbaz')))
-
-    poster = Poster(uri=base_uri, presentation_set=graph.objects())
-
-    expected = [
-        (URIRef('http://example.com/123'), ore.isAggregatedBy, URIRef('http://vocab.lib.umd.edu/set#foobar')),
-        (URIRef('http://example.com/123'), ore.isAggregatedBy, URIRef('http://vocab.lib.umd.edu/set#barbaz'))
-    ]
-
-    for e in expected:
-        assert e in poster.graph
-
-
-# Helper Functions
-
-def create_presentation_set_turtle_format(set_names):
-    preamble = '@prefix ore: <http://www.openarchives.org/ore/terms/> .'
-    preamble = preamble + '@prefix umdset: <http://vocab.lib.umd.edu/set#> .'
-    presentation_set = preamble
-    for set_name in set_names:
-        presentation_set += f'<> ore:isAggregatedBy umdset:{set_name} .'
-    return presentation_set
-
-
-def create_poster_with_presentation_set(item_uri, set_names):
-    presentation_set = create_presentation_set_turtle_format(set_names)
-    return Poster(
-        graph=Graph().parse(data=presentation_set, format='turtle', publicID=base_uri),
-        uri=item_uri
-    )

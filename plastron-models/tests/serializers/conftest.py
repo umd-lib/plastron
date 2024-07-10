e48e5cb8d5f7569faa8a38e1a1863ff6496ba0c1
Peter Eichman <peichman@umd.edu>
2024-07-10 16:03:42 -0400

    LIBFCREPO-1470. Ensure language tagged columns are exported.
    - rename CSVSerializer attribute "rows_by_content_model" to "sheets"
    - created a Sheet class that encapsulates all the per-sheet metadata that the row_info dictionaries did
    - created a ColumnDict class that encapsulates the return value from flatten()
    - moved the write_csv_file() function into the Sheet class as a method
    - after calling flatten(), add the language tagged columns to the "extra_columns" dictionary
    - updated tests
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1470

---
+++ plastron-models/tests/serializers/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from rdflib import Literal
+
+from plastron.models import Item
+
+
+@pytest.fixture
+def header_map():
+    return {
+        'title': 'Title',
+        'subject': {
+            'label': 'Subject',
+            'same_as': 'Subject URI',
+        },
+    }
+
+
+@pytest.fixture
+def multilingual_item():
+    return Item(title=[Literal('The Trial'), Literal('Der Prozeß', lang='de')])
